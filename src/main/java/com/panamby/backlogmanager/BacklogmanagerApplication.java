17edd34f74899bc236bfb590ced0363018021a10
Rafael <rafael.souza@prill.com.br>
2023-09-27 10:00:16 -0300

    Initial commit

---
+++ src/main/java/com/panamby/backlogmanager/BacklogmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.panamby.backlogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panamby/backlogmanager/BacklogmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panamby.backlogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
